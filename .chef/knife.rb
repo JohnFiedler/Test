cce468a855a5c2053f3e413cd1f0eae4d19030b1
John Fiedler <johnfiedler@gmail.com>
2012-08-23 23:39:33 -0500

    .spl redo

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,7 +4,7 @@ node_name                'john'
 client_key               ENV['HOME'] + '/.chef/john.pem'
 validation_client_name   'chef-validator'
 validation_key           ENV['HOME'] + '/.chef/validation.pem'
-chef_server_url          'https://chef-ops.amz.relateiq.com:4000'
+chef_server_url          'http://chef-ops.amz.relateiq.com:4000'
 cache_type               'BasicFile'
 cache_options( :path => ENV['HOME'] + '/.chef/checksums' )
 cookbook_path [ ENV['HOME'] + '/git/chef/cookbooks' ]
@@ -16,7 +16,7 @@ knife[:aws_secret_access_key] = ENV['AWS_SECRET_ACCESS_KEY']
 knife[:aws_ssh_key_id]        = 'us-west-1-chef'
 knife[:availability_zone]     = "us-west-1b"
 knife[:region]                = "us-west-1"
-knife[:image]                 = "ami-ad7054e8" #riq-ami-2012-08-06-164954-x86_64
+knife[:image]                 = "ami-1d2c0858" #riq-ami-2012-08-17
 # knife[:image]                 = "ami-e381a4a6" #riq-ami-2012-08-01-142604-x86_64
 knife[:flavor]                = "m1.small"
 knife[:subnet_id]             = "subnet-3b2aa452"
